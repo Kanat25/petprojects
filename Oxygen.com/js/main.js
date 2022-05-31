96902ff1de3a7d7d25f2a9ba00b4dd944fb171c7
ABK.net <103325595+Kanat25@users.noreply.github.com>
2022-05-31 17:12:46 +0300

    Add files via upload

---
+++ Oxygen.com/js/main.js
@@ -0,0 +1,37 @@
+$(function() {
+
+	// $('.slider__item').slick({
+	// 		dots: true,
+	// 		rtl: true,
+	// 		prevArrow: '<button class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
+	// 		nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>',
+			
+	// });
+    $('.slider__items').slick({
+      dots: true,
+      prevArrow: '<img src="images/arrow-left.svg" class="slick-prev" alt="1">',
+      nextArrow: '<img src="images/arrow-right.svg" class="slick-next" alt="2">',
+          
+    });
+
+    $('.menu__btn').on("click", function(){
+        $('.menu__list').toggleClass('menu__list--active');
+    });
+});
+
+// $('.slider__items').slick({
+//   dots: true,
+//   prevArrow: '<img src="images/arrow-left.svg" class="slick-prev" alt="1">',
+//   nextArrow: '<img src="images/arrow-right.svg" class="slick-next" alt="2">',
+      
+// });
+
+(function () {
+    function setVw() {
+        const vw = document.documentElement.clientWidth / 100;
+        document.documentElement.style.setProperty('--vw', `${vw}px`);
+    }
+
+    setVw();
+    window.addEventListener('resize', setVw);
+}());
